f0d004cf2e1bbc8d65b0285e76a77d95176f4933
Niklas Hansen <nha@3floor.dk>
2025-05-16 13:53:14 +0200

    Add build action (#10)
    * Add build action
    
    * Add XMLDoc

--- src/NovoNordisk.Configuration.AzureBlob/BlobJsonConfigurationSource.cs
+++ src/NovoNordisk.Configuration.AzureBlob/BlobJsonConfigurationSource.cs
@@ -3,14 +3,42 @@ using Microsoft.Extensions.Configuration;
 
 namespace NovoNordisk.Configuration.AzureBlob;
 
+/// <summary>
+/// Configuration source for Azure Blob JSON files.
+/// </summary>
 public class BlobJsonConfigurationSource : IConfigurationSource
 {
+    /// <summary>
+    /// URL for a Azure Blob JSON file.
+    /// </summary>
     public required Uri BlobUrl { get; init; }
+    
+    /// <summary>
+    /// Determines whether to check for file updates periodically.
+    /// </summary>
     public bool ReloadOnChange { get; init; } = false;
+    
+    /// <summary>
+    /// The interval at which the blob is checked for changes. 
+    /// This is only used if <see cref="ReloadOnChange"/> is set to <c>true</c>.
+    /// </summary>
     public TimeSpan PollingInterval { get; init; } = TimeSpan.FromSeconds(30);
+    
+    /// <summary>
+    /// Callback for when any exception occurs during reload.
+    /// </summary>
     public Action<Exception>? ExceptionHandler { get; init; }
+    
+    /// <summary>
+    /// Credentials for accessing Azure Blob Storage. Not needed if public.
+    /// </summary>
     public TokenCredential? Credential { get; init; }
     
+    /// <summary>
+    /// Builds the <see cref="T:NovoNordisk.Configuration.BlobJsonConfigurationProvider" /> for this source.
+    /// </summary>
+    /// <param name="builder">The <see cref="T:Microsoft.Extensions.Configuration.IConfigurationBuilder" />.</param>
+    /// <returns>An <see cref="T:NovoNordisk.Configuration.BlobJsonConfigurationProvider" /></returns>
     public IConfigurationProvider Build(IConfigurationBuilder builder)
     {
         return new BlobJsonConfigurationProvider(this);
--- src/NovoNordisk.Configuration.AzureBlob/ConfigurationBuilderExtensions.cs
+++ src/NovoNordisk.Configuration.AzureBlob/ConfigurationBuilderExtensions.cs
@@ -3,8 +3,21 @@ using Microsoft.Extensions.Configuration;
 
 namespace NovoNordisk.Configuration.AzureBlob;
 
+/// <summary>
+/// Extension methods for adding <see cref="BlobJsonConfigurationProvider"/>.
+/// </summary>
 public static class ConfigurationBuilderExtensions
 {
+    /// <summary>
+    /// Adds a JSON configuration source from an Azure Blob to the configuration builder.
+    /// </summary>
+    /// <param name="builder">The configuration builder to add to.</param>
+    /// <param name="blobUrl">URL to the Azure Blob.</param>
+    /// <param name="reloadOnChange">Whether to reload configuration, if blob changes.</param>
+    /// <param name="pollingInterval">The interval to check for changes.</param>
+    /// <param name="tokenCredential">Credentials to access the blob, if not public.</param>
+    /// <param name="exceptionHandler">Action to be called when an exception occurs during reload.</param>
+    /// <returns>The <see cref="IConfigurationBuilder"/>.</returns>
     public static IConfigurationBuilder AddJsonBlob(this IConfigurationBuilder builder, Uri blobUrl, bool reloadOnChange = false, TimeSpan? pollingInterval = null, TokenCredential? tokenCredential = null, Action<Exception>? exceptionHandler = null)
     {
         return builder.Add(new BlobJsonConfigurationSource
